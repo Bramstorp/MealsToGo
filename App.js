f1168c3376b8815d2babbce9a732b5c4e101409b
Bramstorp <m.bramstorp@stofanet.dk>
2021-03-01 19:37:37 +0100

    update format on app.js

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-} from "react-native";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>Search</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>search</Text>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>List</Text>
+        <View style={styles.list}>
+          <Text>list</Text>
         </View>
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
@@ -26,5 +19,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
 });
