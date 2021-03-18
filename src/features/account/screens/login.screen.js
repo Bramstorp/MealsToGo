49dd2702cd42aa2d09657af95f3d53885f52344c
Bramstorp <m.bramstorp@stofanet.dk>
2021-03-18 17:50:56 +0100

    update to login and base screen

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -5,13 +5,15 @@ import {
   AccountContainer,
   AuthButton,
   AuthInput,
+  ErrorContainer,
+  Title,
 } from "../components/account.styles";
 
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { onLogin, error } = useContext(AuthenticationContext);
@@ -19,6 +21,7 @@ export const LoginScreen = () => {
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthInput
           label="E-mail"
@@ -28,7 +31,7 @@ export const LoginScreen = () => {
           autoCapitalize="none"
           onChangeText={(u) => setEmail(u)}
         />
-        <Spacer size="large">
+        <ErrorContainer size="large">
           <AuthInput
             label="Password"
             value={password}
@@ -38,7 +41,7 @@ export const LoginScreen = () => {
             secure
             onChangeText={(p) => setPassword(p)}
           />
-        </Spacer>
+        </ErrorContainer>
         {error && (
           <Spacer size="large">
             <Text variant="error">{error}</Text>
@@ -54,6 +57,11 @@ export const LoginScreen = () => {
           </AuthButton>
         </Spacer>
       </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
     </AccountBackground>
   );
 };
